d196b6e73264e7a0b5c6dc50ef1107ce0c92a7fe
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2022-04-08 02:26:31 -0600

    Made the chess board and chess pieces on the board

--- src/Frame.cpp
+++ src/Frame.cpp
@@ -67,6 +67,7 @@ void Frame::initBoard()
 {
   board = new wxGridSizer(8, 8, 0, 0);
 
+  // Placing tiles into board
   for (int i = 1; i < 65; i++)
   {
     tiles.push_back(new Tile(this));
@@ -83,7 +84,26 @@ void Frame::initBoard()
     board->Add(tiles[i - 1], 0, wxSHAPED);
   }
 
-  tiles[0]->addPiece("Black", "Knight");
+  wxImage::AddHandler(new wxPNGHandler);
+  // Placing pieces onto the tiles
+  tiles[0]->addPiece("Black", "Rook");
+  tiles[1]->addPiece("Black", "Knight");
+  tiles[2]->addPiece("Black", "Bishop");
+  tiles[3]->addPiece("Black", "Queen");
+  tiles[4]->addPiece("Black", "King");
+  tiles[5]->addPiece("Black", "Bishop");
+  tiles[6]->addPiece("Black", "Knight");
+  tiles[7]->addPiece("Black", "Rook");
+  for (int i = 8; i < 16; i++) tiles[i]->addPiece("Black", "Pawn");
+  tiles[48]->addPiece("White", "Rook");
+  tiles[49]->addPiece("White", "Knight");
+  tiles[50]->addPiece("White", "Bishop");
+  tiles[51]->addPiece("White", "Queen");
+  tiles[52]->addPiece("White", "King");
+  tiles[53]->addPiece("White", "Bishop");
+  tiles[54]->addPiece("White", "Knight");
+  tiles[55]->addPiece("White", "Rook");
+  for (int i = 56; i < 64; i++) tiles[i]->addPiece("White", "Pawn");
 }
 
 bool Frame::inRange(int n, int x, int y) { return (x <= n && n <= y); }
