a63b7211e55623b1eed738e0fdf3d3b6d8981db8
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2022-04-12 20:03:56 -0600

    Okay works on Windows now

--- src/Tile.cpp
+++ src/Tile.cpp
@@ -12,21 +12,21 @@ void Tile::addPiece(wxString colourArg, wxString pieceArg)
 
   if (colourArg == "White")
   {
-    if (pieceArg == "Pawn") image.LoadFile("./img/whitePawn.png");
-    else if (pieceArg == "Knight") image.LoadFile("./img/whiteKnight.png");
-    else if (pieceArg == "Bishop") image.LoadFile("./img/whiteBishop.png");
-    else if (pieceArg == "Rook") image.LoadFile("./img/whiteRook.png");
-    else if (pieceArg == "Queen") image.LoadFile("./img/whiteQueen.png");
-    else if (pieceArg == "King") image.LoadFile("./img/whiteKing.png");
+    if (pieceArg == "Pawn")        image.LoadFile("./img/whitePawn.png", wxBITMAP_TYPE_PNG);
+    else if (pieceArg == "Knight") image.LoadFile("./img/whiteKnight.png", wxBITMAP_TYPE_PNG);
+    else if (pieceArg == "Bishop") image.LoadFile("./img/whiteBishop.png", wxBITMAP_TYPE_PNG);
+    else if (pieceArg == "Rook")   image.LoadFile("./img/whiteRook.png", wxBITMAP_TYPE_PNG);
+    else if (pieceArg == "Queen")  image.LoadFile("./img/whiteQueen.png", wxBITMAP_TYPE_PNG);
+    else if (pieceArg == "King")   image.LoadFile("./img/whiteKing.png", wxBITMAP_TYPE_PNG);
   }
   else if (colourArg == "Black")
   {
-    if (pieceArg == "Pawn") image.LoadFile("./img/blackPawn.png");
-    else if (pieceArg == "Knight") image.LoadFile("./img/blackKnight.png");
-    else if (pieceArg == "Bishop") image.LoadFile("./img/blackBishop.png");
-    else if (pieceArg == "Rook") image.LoadFile("./img/blackRook.png");
-    else if (pieceArg == "Queen") image.LoadFile("./img/blackQueen.png");
-    else if (pieceArg == "King") image.LoadFile("./img/blackKing.png");
+    if (pieceArg == "Pawn")        image.LoadFile("./img/blackPawn.png", wxBITMAP_TYPE_PNG);
+    else if (pieceArg == "Knight") image.LoadFile("./img/blackKnight.png", wxBITMAP_TYPE_PNG);
+    else if (pieceArg == "Bishop") image.LoadFile("./img/blackBishop.png", wxBITMAP_TYPE_PNG);
+    else if (pieceArg == "Rook")   image.LoadFile("./img/blackRook.png", wxBITMAP_TYPE_PNG);
+    else if (pieceArg == "Queen")  image.LoadFile("./img/blackQueen.png", wxBITMAP_TYPE_PNG);
+    else if (pieceArg == "King")   image.LoadFile("./img/blackKing.png", wxBITMAP_TYPE_PNG);
   }
 
   if (!image.IsOk()) Close();
